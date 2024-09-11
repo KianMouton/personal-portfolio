184302da1f86114405876ac34315ab0136ceaa0e
KianMouton <kianton2004@gmail.com>
2024-09-11 16:43:58 +0200

    gsap is confusion

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,18 +1,43 @@
+import React, { useState } from 'react';
 import projectImage1 from "../images/project_img_1.jpg";
+import projectImage2 from "../images/project_img_2.jpg";
+import projectImage3 from "../images/project_img_3.jpg";
+import projectImage4 from "../images/project_img_4.jpg";
+import { gsap } from 'gsap';
 
 const Projects = () => {
+    const images = [projectImage4, projectImage3, projectImage2, projectImage1];
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const handleNextImage = () => {
+        const imgElement = document.querySelector('.project-img');
+
+        // Update the index immediately
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+
+        // Animate the image out, then in
+        gsap.to(imgElement, { opacity: 0, onComplete: () => {
+            // Change the source of the image after fade-out
+            imgElement.src = images[(currentIndex + 1) % images.length];
+            gsap.to(imgElement, { opacity: 1 });
+        }});
+    };
+
     return (
         <div className='projects'>
             <h1>Projects</h1>
             <div className='projects-container'>
                 <div className='project'>
-                    <h2>Eccomerce Store</h2>
-                    <img src={projectImage1} alt='project img 1' />
+                    <h2>E-commerce Store</h2>
+                    <div className='img-wrapper'>
+                        <img className='project-img' src={images[currentIndex]} alt={`project img ${currentIndex + 1}`} />
+                    </div>
+                    <button className='img-btn' onClick={handleNextImage}>Next Image</button>
                     <p>A responsive e-commerce store built with Mongo, Express, React and Node.js</p>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Projects;
