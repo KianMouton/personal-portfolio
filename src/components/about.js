e6f65c5067657e2616b1edfe8e28a5c7dba3082e
KianMouton <kianton2004@gmail.com>
2024-09-14 22:33:25 +0200

    need to make some changes to the grid

--- src/components/about.js
+++ src/components/about.js
@@ -2,15 +2,15 @@
 const About = () => {
 
     const scrollToElement = (event) => {
-        event.preventDefault(); // Prevent the default anchor click behavior
+        event.preventDefault(); 
 
-        const targetId = event.currentTarget.getAttribute('href').substring(1); // Get the target ID from href
-        const targetElement = document.getElementById(targetId); // Find the target element
+        const targetId = event.currentTarget.getAttribute('href').substring(1); 
+        const targetElement = document.getElementById(targetId); 
 
         if (targetElement) {
             targetElement.scrollIntoView({
-                behavior: 'smooth', // Enables smooth scrolling
-                block: 'start' // Aligns the top of the element to the top of the viewport
+                behavior: 'smooth', 
+                block: 'start' 
             });
         }
     };
@@ -22,11 +22,19 @@ const About = () => {
                 <h1>Kian Mouton</h1>
                 <p>and I am a Full-Stack Developer</p>
             </div>
-            <div className='nav'>
-                <a href='#me' onClick={scrollToElement}><p>about</p></a>
-                <a href='#skills' onClick={scrollToElement}><p>skills</p></a>
-                <a href='#projects' onClick={scrollToElement}><p>projects</p></a>
-            </div>
+            <nav className='nav'>
+                <a href='#me' onClick={scrollToElement}><button>about</button></a>
+                <a href='#skills' onClick={scrollToElement}><button>skills</button></a>
+                <a href='#projects' onClick={scrollToElement}><button>projects</button></a>
+                <div className='links'>
+                <a href="https://github.com/KianMouton" target="_blank" rel="noopener noreferrer">
+                    <i className="fa-brands fa-github fa-2xl"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/kian-mouton-4797ba2b7/" target="_blank" rel="noopener noreferrer">
+                    <i className="fa-brands fa-linkedin fa-2xl"></i>
+                </a>
+                </div>
+            </nav>
         </div>
     )
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+
 import projectImage1 from "../images/project_img_1.jpg";
 import projectImage2 from "../images/project_img_2.jpg";
 import projectImage3 from "../images/project_img_3.jpg";
@@ -9,97 +9,15 @@ import drumPad from "../images/drumpad.png";
 import { gsap } from 'gsap';
 
 const Projects = () => {
-    const Storeimages = [projectImage1, projectImage2, projectImage3, projectImage4];
-    const CalImages = [calculator1, calculator2];
-    const [StoreCurrentIndex, setStoreCurrentIndex] = useState(0);
-    const [calCurrentIndex, setCalCurrentIndex] = useState(0);
-
-    const handleNextStore = () => {
-        const imgElement = document.querySelector('.project-img.store');
-
-        gsap.to(imgElement, { 
-            duration: 0.2,  
-            filter: 'blur(3px)', 
-            onComplete: () => {
-               
-                setStoreCurrentIndex((prevIndex) => {
-                    const newIndex = (prevIndex + 1) % Storeimages.length;
-                    imgElement.src = Storeimages[newIndex]; 
-                    return newIndex; 
-                });
-
-                gsap.to(imgElement, { 
-                    duration: 0.2, 
-                    filter: 'blur(0px)' 
-                });
-            }
-        });
-    };
-
-    const handleNextCal = () => {
-        const imgElement = document.querySelector('.project-img.cal');
-
-        gsap.to(imgElement, { 
-            duration: 0.2,  
-            filter: 'blur(3px)', 
-            onComplete: () => {
-                
-                setCalCurrentIndex((prevIndex) => {
-                    const newIndex = (prevIndex + 1) % CalImages.length;
-                    imgElement.src = CalImages[newIndex];
-                    return newIndex; 
-                });
-
-                gsap.to(imgElement, { 
-                    duration: 0.2, 
-                    filter: 'blur(0px)' 
-                });
-            }
-        });
-    };
+    
 
     return (
         <div className='projects' id='projects'>
             <h1>Projects</h1>
-            <div className='projects-container'>
-                <div className='project'>
-                    <div className='project-title'>
-                    <h2>E-commerce Store</h2>
-                    <div className='img-wrapper'>
-                        <img className='project-img store' src={Storeimages[StoreCurrentIndex]} alt={`project img ${StoreCurrentIndex + 1}`} />
-                    </div>
-                    <button className='img-btn' onClick={handleNextStore}>Next Image</button>
-                    </div>
-                    <div className='description'>
-                    <p>A responsive e-commerce store built with Mongo, Express, React, Node.js and using Yoco for payment processing</p>
-                    <a className='view-code-btn' href='https://github.com/KianMouton/e-commerce-store' target='_blank' rel='noopener noreferrer'>View Code</a>
-                    </div>
-                </div>
-                <div className='project'>
-                    <div className='project-title'>
-                    <h2>Calculator App</h2>
-                    <div className='img-wrapper'>
-                        <img className='project-img cal' src={CalImages[calCurrentIndex]} alt={`project img ${calCurrentIndex + 1}`} />
-                    </div>
-                    <button className='img-btn' onClick={handleNextCal}>Next Image</button>
-                    </div>
-                    <div className='description'>
-                    <p>A Calculator web App</p>
-                    <a className='view-code-btn' href='https://github.com/KianMouton/e-commerce-store' target='_blank' rel='noopener noreferrer'>View Code</a>
-                    </div>
-                </div>
-                <div className='project'>
-                    <div className='project-title'>
-                    <h2>Drum Pad App</h2>
-                    <div className='img-wrapper'>
-                        <img className='project-img' src={drumPad} alt='project img 1' />
-                    </div>
-                    </div>
-                    <div className='description'>
-                    <p>A Drum pad built using React</p>
-                    <a className='view-code-btn' href='https://github.com/KianMouton/e-commerce-store' target='_blank' rel='noopener noreferrer'>View Code</a>
-                    </div>
-                </div>
+            <div className='projects-grid'>
+                <img src={projectImage1} alt='projectImage1'/>
+                <img src={calculator1} alt='calculator1'/>
+                <img src={drumPad} alt='drumPad'/>
             </div>
         </div>
     );
