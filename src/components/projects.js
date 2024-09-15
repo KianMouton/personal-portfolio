2d8ae4f6ab9c49fc10aaaa682cf565722b50fc13
KianMouton <kianton2004@gmail.com>
2024-09-15 02:03:13 +0200

    fixed the projects section

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,23 +1,45 @@
-
 import projectImage1 from "../images/project_img_1.jpg";
-import projectImage2 from "../images/project_img_2.jpg";
-import projectImage3 from "../images/project_img_3.jpg";
-import projectImage4 from "../images/project_img_4.jpg";
 import calculator1 from "../images/calculator_1.png";
-import calculator2 from "../images/calculator_2.png";
 import drumPad from "../images/drumpad.png";
-import { gsap } from 'gsap';
 
 const Projects = () => {
-    
-
     return (
         <div className='projects' id='projects'>
             <h1>Projects</h1>
             <div className='projects-grid'>
-                <img src={projectImage1} alt='projectImage1'/>
-                <img src={calculator1} alt='calculator1'/>
-                <img src={drumPad} alt='drumPad'/>
+                <div className='project-item'>
+                    <img src={projectImage1} alt='projectImage1' />
+                    <div className='overlay'>
+                        <p>Eccomerce Store</p>
+                        <a href="https://github.com/KianMouton/mern-store" target="_blank" rel="noopener noreferrer">
+                            <i className="fa-brands fa-github fa-2xl"></i>
+                        </a>
+                    </div>
+                </div>
+                <div className='project-item'>
+                    <img src={calculator1} alt='calculator1' />
+                    <div className='overlay'>
+                        <p>Calculator App</p>
+                        <a href="https://github.com/KianMouton/react-calculator" target="_blank" rel="noopener noreferrer">
+                            <i className="fa-brands fa-github fa-2xl"></i>
+                        </a>
+                        <a href='https://kianmouton.github.io/react-calculator/' target="_blank" rel="noopener noreferrer">
+                            <i className='fa-solid fa-arrow-up-right-from-square' />
+                        </a>
+                    </div>
+                </div>
+                <div className='project-item'>
+                    <img src={drumPad} alt='drumPad' />
+                    <div className='overlay'>
+                        <p>Drum Pad App</p>
+                        <a href="https://github.com/KianMouton/react-Drum-Pad" target="_blank" rel="noopener noreferrer">
+                            <i className="fa-brands fa-github fa-2xl"></i>
+                        </a>
+                        <a href='https://kianmouton.github.io/react-Drum-Pad/' target="_blank" rel="noopener noreferrer">
+                            <i className='fa-solid fa-arrow-up-right-from-square' />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     );
